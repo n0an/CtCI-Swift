343a5695f0e2bcb46ab8b695c0f6806433463b15
n0an <nantog@gmail.com>
2019-04-24 17:48:37 +0300

    BitManipulationTests added

---
+++ CtCI_TasksTests/BitManipulationTests.swift
@@ -0,0 +1,24 @@
+//
+//  BitManipulationTests.swift
+//  CtCI_TasksTests
+//
+//  Created by nag on 24/04/2019.
+//  Copyright © 2019 Anton Novoselov. All rights reserved.
+//
+
+import XCTest
+
+class BitManipulationTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+    
+    // * 5.1. Insertion
+    func test5_1_insertion() {
+        
+        
+    }
+    
+
+}
